9b86788dedcef647bfbb8fb82f6e69c74b0d1f85
Mikołaj Pich <m.pich@outlook.com>
2016-10-31 22:28:26 +0100

    Add gui for pass_gen

--- pass_gen.php
+++ pass_gen.php
@@ -1,3 +1,30 @@
-<?php
-
-echo password_hash('pass', PASSWORD_DEFAULT);
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Hash generator</title>
+    <link rel="stylesheet" href="web/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+    <section class="container">
+      <div class="page-header">
+        <h1>Generate your password hash</h1>
+      </div>
+      <form action="" method="post" class="form-horizontal">
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="password" class="form-control" name="password"
+            value="<?=$_POST['password'];?>" autofocus required>
+          </div>
+        </div>
+        <button class="btn btn-primary" name="submit">Generate!</button>
+      </form>
+      <?php if (isset($_POST['password'])): ?>
+        <hr>
+        <input type="text" class="form-control"
+          value="<?=password_hash($_POST['password'], PASSWORD_DEFAULT);?>">
+        <p class="text-muted">It's never be the same.</p>
+      <?php endif ?>
+    </section>
+  </body>
+</html>
