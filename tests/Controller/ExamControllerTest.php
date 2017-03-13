78506d13b83c0ff9c78bee25a9a496a46f5e8b89
Mikołaj Pich <m.pich@outlook.com>
2017-03-14 00:59:35 +0100

    Add exam tests

---
+++ tests/Controller/ExamControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Egzaminer\Controller\ExamController;
+
+class ExamControllerTest extends EgzaminerTestsControllerTestCase
+{
+    /**
+     * @expectedException Exception
+     */
+    public function testShowActionWhenExamNoExist()
+    {
+        $controller = new ExamController($this->container);
+
+        $this->assertInternalType('string', $controller->showAction(1));
+    }
+
+    public function testShowAction()
+    {
+    	self::$pdo->exec("INSERT INTO exams (id, title, questions, threshold, group_id)
+    		VALUES (NULL, 'Test', 16, 7, 1)");
+        $controller = new ExamController($this->container);
+
+        $controller->showAction(1);
+    }
+}
